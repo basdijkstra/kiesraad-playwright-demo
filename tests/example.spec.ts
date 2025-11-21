283c089725425d95871b36d06d6c29f9ec3df897
Bas Dijkstra <bas@ontestautomation.com>
2025-11-21 11:38:35 +0100

    Use describe block

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,31 +1,65 @@
 import { test, expect } from '@playwright/test';
 
-test('User can buy a Sauce Labs Backpack', async ({ page }) => {
+test.describe('User can buy products from the shop', async() => {
 
-  await page.goto('https://www.saucedemo.com');
+  test('User can buy a Sauce Labs Backpack', async ({ page }) => {
 
-  await page.getByPlaceholder('Username').fill('standard_user');
-  await page.getByPlaceholder('Password').fill('secret_sauce');
-  await page.getByRole('button', { name: 'Login' }).click();
+    await page.goto('https://www.saucedemo.com');
 
-  await expect(page.getByTestId('shopping-cart-link')).toBeVisible();
+    await page.getByPlaceholder('Username').fill('standard_user');
+    await page.getByPlaceholder('Password').fill('secret_sauce');
+    await page.getByRole('button', { name: 'Login' }).click();
 
-  await page.getByText('Sauce Labs Backpack').click();
+    await expect(page.getByTestId('shopping-cart-link')).toBeVisible();
 
-  await expect(page.getByTestId('back-to-products')).toBeVisible();
+    await page.getByText('Sauce Labs Backpack').click();
 
-  await page.getByText('Add to cart').click();
+    await expect(page.getByTestId('back-to-products')).toBeVisible();
 
-  await page.getByTestId('shopping-cart-link').click();
+    await page.getByText('Add to cart').click();
 
-  await page.getByRole('button', { name: 'Checkout'}).click();
+    await page.getByTestId('shopping-cart-link').click();
 
-  await page.getByPlaceholder('First Name').fill('Bas');
-  await page.getByPlaceholder('Last Name').fill('Dijkstra');
-  await page.getByPlaceholder('Zip/Postal Code').fill('12345');
+    await page.getByRole('button', { name: 'Checkout'}).click();
 
-  await page.getByRole('button', { name: 'Continue'}).click();
-  await page.getByRole('button', { name: 'Finish'}).click();
+    await page.getByPlaceholder('First Name').fill('Bas');
+    await page.getByPlaceholder('Last Name').fill('Dijkstra');
+    await page.getByPlaceholder('Zip/Postal Code').fill('12345');
 
-  await expect(page.getByText('Thank you for your order!')).toBeVisible();
+    await page.getByRole('button', { name: 'Continue'}).click();
+    await page.getByRole('button', { name: 'Finish'}).click();
+
+    await expect(page.getByText('Thank you for your order!')).toBeVisible();
+  });
+
+  test('User can buy a Sauce Labs Bike Light', async ({ page }) => {
+
+    await page.goto('https://www.saucedemo.com');
+
+    await page.getByPlaceholder('Username').fill('standard_user');
+    await page.getByPlaceholder('Password').fill('secret_sauce');
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    await expect(page.getByTestId('shopping-cart-link')).toBeVisible();
+
+    await page.getByText('Sauce Labs Bike Light').click();
+
+    await expect(page.getByTestId('back-to-products')).toBeVisible();
+
+    await page.getByText('Add to cart').click();
+
+    await page.getByTestId('shopping-cart-link').click();
+
+    await page.getByRole('button', { name: 'Checkout'}).click();
+
+    await page.getByPlaceholder('First Name').fill('Bas');
+    await page.getByPlaceholder('Last Name').fill('Dijkstra');
+    await page.getByPlaceholder('Zip/Postal Code').fill('12345');
+
+    await page.getByRole('button', { name: 'Continue'}).click();
+    await page.getByRole('button', { name: 'Finish'}).click();
+
+    await expect(page.getByText('Thank you for your order!')).toBeVisible();
+  });
+
 });
